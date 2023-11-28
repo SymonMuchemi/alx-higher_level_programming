11f884fb1221f45ff2c9f75dcc6edde2820e2034
SymonMuchemi <simonkenya846@gmail.com>
2023-11-29 01:27:06 +0300

    End with no comma

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-# print all numbers from 0 to 98
+# print all numbers from 0 to 99
 # prefix all nunmbers less than 10 with a zero
 
-for i in range(0, 99):
-    print("{}".format(str(i).zfill(2)), end=", ")
+for i in range(0, 100):
+    if i < 99:
+        print("{}".format(str(i).zfill(2)), end=", ")
+    else:
+        print("{}".format(i), end="")
