dbc698cbf1c1161440d28883e9f29d6a29ed9322
SymonMuchemi <simonkenya846@gmail.com>
2023-12-02 00:32:05 +0300

    Tweaks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 # This program prints the arguments passed to it
 if __name__ == "__main__":
-    import sys
-    argc = len(sys.argv)
-    if argc == 1:
-        print("{:d} arguments.".format(argc - 1))
-    elif argc > 2:
+    from sys import argv
+    argc = len(argv) - 1
+    if argc == 0:
+        print("{:d} arguments.".format(argc))
+    elif argc > 1:
         print("{:d}: arguments:".format(argc))
-        for i in range(1, argc):
-            print("{:d}: {}".format(i, sys.argv[i]))
+        for i in range(1, argc + 1):
+            print("{:d}: {}".format(i, argv[i]))
     else:
         argc = 1
         print("{:d} argument:".format(argc))
-        print("{:d}: {}".format(argc, sys.argv[1]))
+        print("{:d}: {}".format(argc, argv[1]))
