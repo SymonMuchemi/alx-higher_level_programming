e14bef828466769f2808b560fd9eb29ed3259b5a
SymonMuchemi <simonkenya846@gmail.com>
2023-12-02 01:57:30 +0300

    Lines

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 # This program emulates a simple calculator
-from calculator_1 import add, sub, mul, div
-
-def calculate(a, op, b):
-    """handles simple calculations
-
-    Args:
-        a (int): first operand
-        op (char): operator
-        b (int): second operand
-    """
-    a = int(a)
-    b = int(b)
-    match op:
-        case "+":
-            print("{:d} {} {:d} = {:d}".format(a, op, b, add(a, b)))
-        case "-":
-            print("{:d} {} {:d} = {:d}".format(a, op, b, sub(a, b)))
-        case "*":
-            print("{:d} {} {:d} = {:d}".format(a, op, b, mul(a, b)))
-        case "*":
-            print("{:d} {} {:d} = {:d}".format(a, op, b, div(a, b)))
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-
 if __name__ == "__main__":
-    import sys
+    from calculator_1 import add, sub, mul, div
     from sys import argv
+    import sys
     argc = len(argv)
+    def calculate(a, op, b):
+        """handles simple calculations
+
+        Args:
+            a (int): first operand
+            op (char): operator
+            b (int): second operand
+        """
+        a = int(a)
+        b = int(b)
+        match op:
+            case "+":
+                print("{:d} {} {:d} = {:d}".format(a, op, b, add(a, b)))
+            case "-":
+                print("{:d} {} {:d} = {:d}".format(a, op, b, sub(a, b)))
+            case "*":
+                print("{:d} {} {:d} = {:d}".format(a, op, b, mul(a, b)))
+            case "*":
+                print("{:d} {} {:d} = {:d}".format(a, op, b, div(a, b)))
+            case _:
+                print("Unknown operator. Available operators: +, -, * and /")
+
     if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
