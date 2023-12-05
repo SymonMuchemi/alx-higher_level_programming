8ad0a91d0dc335087c5cac28827f2492bc286a0e
SymonMuchemi <simonkenya846@gmail.com>
2023-12-06 00:42:22 +0300

    pycodestyle behaving like betty

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,7 +4,7 @@ def print_list_integer(my_list=[]):
     """print a list of integers
 
     Args:
-        my_list (list, optional): the list of integers to be printed. Defaults to [].
+        my_list (list, optional): the list of integers. Defaults to [].
     """
     for item in my_list:
         print("{:d}".format(item))
