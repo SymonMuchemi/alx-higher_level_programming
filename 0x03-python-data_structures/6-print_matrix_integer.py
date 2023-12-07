e1a6ab334d7be8087957b6dd6e6538d0471a309d
SymonMuchemi <simonkenya846@gmail.com>
2023-12-07 23:55:08 +0300

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """prints a matrix of integers
+
+    Args:
+        matrix (list, optional): list of lists. 
+        Defaults to [[]].
+    """
+    for row in matrix:
+        for column in row:
+            print("{:d} ".format(column),  end=' ' if column != row[-1] else '')
+        print("$")
