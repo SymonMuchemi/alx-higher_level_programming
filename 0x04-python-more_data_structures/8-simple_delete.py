36ee7ebe11a2912dcc26b9298bec93a3e428f811
SymonMuchemi <simonkenya846@gmail.com>
2023-12-10 01:07:22 +0300

    inline documentation

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    """deletes a key in a dictionary
+
+    Args:
+        a_dictionary (dict): the dictionary
+        key (str, optional): the key to delete. Defaults to "".
+
+    Returns:
+        dict: the new dictionary
+    """
     if key in a_dictionary:
         a_dictionary.pop(key)
         return a_dictionary
