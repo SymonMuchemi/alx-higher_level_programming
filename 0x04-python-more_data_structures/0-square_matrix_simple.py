64fb517c9bc8eeedb4d5c3ae05a41af5cc22585a
SymonMuchemi <simonkenya846@gmail.com>
2023-12-09 22:54:38 +0300

    Task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    """squares all elements in amtrix
+
+    Args:
+        matrix (list, optional): two dimensional array. 
+                                    Defaults to [].
+    Returns:
+        list: matrix containing squares
+    """
+    if len(matrix) == 0:
+        return matrix
+    else:
+        new_matrix = []
+        for i in range(len(matrix)):
+            new_matrix.append(list(map(lambda x: x ** 2, matrix[i])))
+        return new_matrix
