0427db497e23e14e44014fa8e527466049b684a1
SymonMuchemi <simonkenya846@gmail.com>
2023-12-19 01:19:40 +0300

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """prints x number of elements in a list
+
+    Args:
+        my_list (list, optional): the list of elements. Defaults to [].
+        x (int, optional): the number of elements. Defaults to 0.
+    Return:
+        the real number of elements printed
+    """
+    i = 0
+    num_str = ""
+    while ((i < x) and (x > 0)) :
+        try:
+            num_str += str(my_list[i])
+            i += 1
+        except IndexError:
+            break
+    print("{}".format(num_str))
+    return (i)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,11 @@
+# 0x05. Python - Exceptions
+
+In this repository, I have code that express my understanding in the following topics concerning python:
+
+    - the difference between errors and exceptions
+    - how to use exceptions
+    - the significance of exceptions
+    - handling exceptions correctly
+    - purpose of catching an exceptions
+    - raising a builtin exception
+    - implementing a clean-up action after an exception
