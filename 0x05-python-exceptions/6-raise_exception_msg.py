59946c7ce856b12c08f2441fdb12f1a02f041be1
SymonMuchemi <simonkenya846@gmail.com>
2023-12-20 18:34:41 +0300

    named exception

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """raises a named exception
+
+    Args:
+        message (str, optional): name of the error. Defaults to "".
+
+    Raises:
+        NameError: _description_
+    """
+    raise NameError(message)
