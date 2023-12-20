7b2a9d1b3baf6f0018395b2869e60a503b4f5566
SymonMuchemi <simonkenya846@gmail.com>
2023-12-20 18:17:43 +0300

    using for loop

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,24 +1,4 @@
 #!/usr/bin/python3
-def convert_to_integers(lst):
-    """converts list elements into integers
-
-    Args:
-        lst (any): the array of elements
-
-    Returns:
-        list: the final list of integer and or None elements
-    """
-    final_list = []
-    for item in lst:
-        try:
-            item = int(item)
-        except (ValueError, TypeError):
-            item = None
-        finally:
-            final_list.append(item)
-    return final_list
-
-
 def list_division(my_list_1, my_list_2, list_length):
     """divides the elements of two lists
 
@@ -30,14 +10,12 @@ def list_division(my_list_1, my_list_2, list_length):
     Returns:
         list: result of element division
     """
-    lst1 = convert_to_integers(my_list_1)
-    lst2 = convert_to_integers(my_list_2)
     final_list = []
     i = 0
     # max_len = max(len(my_list_1), len(my_list_2))
     for i in range(0, list_length):
         try:
-            result = (lst1[i] / lst2[i])
+            result = (my_list_1[i] / my_list_2[i])
         except (ZeroDivisionError):
             result = 0
             print("division by 0")
