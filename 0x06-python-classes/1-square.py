03b90789ab07cdf56e9db43169f62f38eb157231
SymonMuchemi <simonkenya846@gmail.com>
2023-12-29 23:52:43 +0300

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""square
+
+This module creates a simple square.
+
+Attributes:
+    size: this is the length of each side of the square
+"""
+
+
+class Square:
+    def __init__(self, size):
+      self.__size = size
