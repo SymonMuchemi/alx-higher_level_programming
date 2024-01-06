ede50724604400442bfc6628b1c805b48e3ab441
SymonMuchemi <simonkenya846@gmail.com>
2024-01-06 04:13:04 +0300

    3 lines

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-"""Function to copy a list
-    """
-
 def copy_list(l):
-    """clones a list
-
-    Args:
-        l (list): array of elements
-
-    Returns:
-        list: the cloned array
-    """
-    return l.copy()
+    return (l.copy())
