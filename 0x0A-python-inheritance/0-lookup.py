a671c1318d78c5f9002674205423648275d60c00
SymonMuchemi <simonkenya846@gmail.com>
2024-01-11 02:32:07 +0300

    test on lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,15 @@
 """
 
 def lookup(obj):
+    """return a list of available attributes and methods in an 
+    object
+
+    Args:
+        obj (object): the class
+
+    Returns:
+        list: methods and attributes
+    """
     if obj is not None:
         return (dir(obj))
     return None
