ae85805dae4ff008730901e6664fad10dc450b1d
SymonMuchemi <simonkenya846@gmail.com>
2024-01-11 23:35:34 +0300

    Two lines on top of class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """basic geometry class parents Rectangle"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """This class inherits from BaseGeometry"""
     def __init__(self, width, height):
