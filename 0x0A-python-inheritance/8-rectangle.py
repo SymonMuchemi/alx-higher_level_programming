e551796cb9a6c99ad6255a6bea653efa94a2c133
SymonMuchemi <simonkenya846@gmail.com>
2024-01-11 23:18:52 +0300

    white spaces

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -37,4 +37,4 @@ class Rectangle(BaseGeometry):
         """
         self.__width = self.integer_validator("width", width)
         self.__height = self.integer_validator("height", height)
-        
+
