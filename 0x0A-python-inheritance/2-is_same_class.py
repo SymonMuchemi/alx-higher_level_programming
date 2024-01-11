ff2ec87151a1b9b613935e0fed1aa88bd2487fe0
SymonMuchemi <simonkenya846@gmail.com>
2024-01-11 18:30:20 +0300

    doc and use is

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""program to check parent instance relationship
+"""
+
 
 def is_same_class(obj, a_class):
     """checks if an object is exactly an instance of the specified class
@@ -10,4 +13,4 @@ def is_same_class(obj, a_class):
     Returns:
         True or False: if the object is an instance of the class
     """
-    return (type(obj) == a_class)
+    return (type(obj) is a_class)
