2dec0fc0d3a36b26d9217c583e96cd5c0943e23f
SymonMuchemi <simonkenya846@gmail.com>
2024-01-13 23:00:56 +0300

    checked if file exists first

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,13 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 file_name = "add_item.json"
-obj_list = []
 
-if (len(sys.argv) > 1):
-    for i in range(1, len(sys.argv)):
-        obj_list.append(sys.argv[i])
+try:
+    items = load_from_json_file(file_name)
+except FileNotFoundError:
+    obj_list = []
+    if (len(sys.argv) > 1):
+        for i in range(1, len(sys.argv)):
+            obj_list.append(sys.argv[i])
 
-save_to_json_file(obj_list, file_name)
+    save_to_json_file(obj_list, file_name)
