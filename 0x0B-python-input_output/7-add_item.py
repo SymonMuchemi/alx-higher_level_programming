f6983dfc35e570fee9e8814a0231eecd050df8c1
SymonMuchemi <simonkenya846@gmail.com>
2024-01-13 23:11:33 +0300

    double trouble

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,18 +2,20 @@
 """program that adds all command line arguments and saves to a file"""
 import sys
 
-if __name__ = "__main__":
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    # Importing functions from other modules
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-    file_name = "add_item.json"
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        items = load_from_json_file(file_name)
+        # Attempt to load existing items from the "add_item.json" file
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        obj_list = []
-        if (len(sys.argv) > 1):
-            for i in range(1, len(sys.argv)):
-                obj_list.append(sys.argv[i])
+        # If the file is not found, create an empty list
+        items = []
 
-    save_to_json_file(obj_list, file_name)
+    # Extend the 'items' list with the command-line arguments (excluding the script name at sys.argv[0])
+    items.extend(sys.argv[1:])
+
+    # Save the updated 'items' list back to the "add_item.json" file
+    save_to_json_file(items, "add_item.json")
