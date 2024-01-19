345489c69ca042e2dd240e5ecd45eeb76da6491f
SymonMuchemi <simonkenya846@gmail.com>
2024-01-19 22:06:50 +0300

    white spaces on base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """a base class"""
 
+
 class Base:
     """base class that manages the number of instances
     and avoid duplication
     """
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
