e4c99e64759e2a34ec67e68456bfa3412e057c5f
symonmuchemi <simonkenya846@gmail.com>
2024-02-16 04:59:46 +0300

    order by number

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,4 +2,5 @@
 -- the result should display:
     -- the score
     -- the number of records for this 'score' with the label 'number'
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number
+DESC;
