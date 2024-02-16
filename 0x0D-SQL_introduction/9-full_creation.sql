4b4e775fa78c70fdcc7431e0053399c829585f7b
symonmuchemi <simonkenya846@gmail.com>
2024-02-16 04:23:42 +0300

    creating second table and adding data

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- creates a table 'second table' and adds multiple rows
+CREATE TABLE IF NOT EXISTS second_table ( id INT, name VARCHAR(256), score INT);
+
+-- adding values to the newly created table
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
