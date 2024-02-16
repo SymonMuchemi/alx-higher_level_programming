7606f441a1c8f822baab14019d007b91278cc6c0
symonmuchemi <simonkenya846@gmail.com>
2024-02-16 04:31:14 +0300

    displaying score and name ordered by the highest to the lowest score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table 
+-- Conditions:
+--  display the score followed by the name 
+--  record should be ordered by the score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
