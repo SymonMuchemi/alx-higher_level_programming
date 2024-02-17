43a6a09bd945a6855edbabd31cf0eac36271bd89
symonmuchemi <simonkenya846@gmail.com>
2024-02-17 19:42:34 +0300

    create user 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- create a user with only read access to database
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- create a new database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
