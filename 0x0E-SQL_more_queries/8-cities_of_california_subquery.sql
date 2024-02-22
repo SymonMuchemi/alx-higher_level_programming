df32f3e2ee193e04a2393fb457d263e6335227de
symonmuchemi <simonkenya846@gmail.com>
2024-02-22 04:51:41 +0300

    inside bracket query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,9 @@
 -- cities of califonia subquery
-SELECT cities.id, cities.state_id, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
-AND states.name = 'California'
-ORDER BY cities.id ASC;
+SELECT id, name 
+FROM cities 
+WHERE state_id = 
+(
+    SELECT id 
+    FROM states 
+    WHERE name = "California"
+) ORDER BY id; 
