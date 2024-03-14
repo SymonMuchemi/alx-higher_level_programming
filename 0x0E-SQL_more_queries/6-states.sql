f08a522050b4109a03d5f2172afe87f72fa64434
SymonMuchemi <simonkenya846@gmail.com>
2024-03-14 03:49:03 +0300

    unique, states and cities

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- column with unique, not null and primary key
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (256)
+);
