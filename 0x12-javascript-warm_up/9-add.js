19f37bc6b20cbc20fc8399042ec9e0d2907c5174
SymonMuchemi <simonkenya846@gmail.com>
2024-04-09 00:20:02 +0300

    Task 9: fix
    used only func

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
-// adds two integer arguments
-const num1 = process.argv[2];
-const num2 = process.argv[3];
+// function to add two numbers
+function add(a, b) {
+  const x = a + b;
 
-// function to add the two numbers
-function add (a, b) { return (a + b); }
-
-if (isNaN(num1) === false && isNaN(num2) === false) {
-  const a = parseInt(num1);
-  const b = parseInt(num2);
-
-  console.log(add(a, b));
+  console.log(x);
 }
+
+add(Number(process.argv[2]), Number(process.argv[3]));
+
