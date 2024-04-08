d22b2923b319f24c668e9d36dc7901d4c232d819
SymonMuchemi <simonkenya846@gmail.com>
2024-04-09 00:37:56 +0300

    Task 10
    Finding the factorial of a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// finds the factorial of a number
+function fact(a) {
+  let result = 1;
+
+  if (a === 0 || a === 1) return 1;
+
+  return (a * fact(a - 1));
+}
+
+console.log(fact(Number(process.argv[2])));
