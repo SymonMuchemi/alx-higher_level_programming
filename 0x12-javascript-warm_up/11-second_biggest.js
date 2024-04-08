0172cfbc1b3e5efa2f58a668155bcef1db43bcb2
SymonMuchemi <simonkenya846@gmail.com>
2024-04-09 01:13:41 +0300

    Task 11:fix
    Getting the second biggest integer in the list of command line arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,8 @@
 #!/usr/bin/node
-
-// finds the second largest integer argument
 if (process.argv.length <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
-  const intArr = [];
-
-  for (let i = 3; i < process.argv.length; i++) {
-    const num = process.argv[i];
-
-    if (!isNaN(num)) intArr.push(num);
-  }
-
-  intArr.sort((a, b) => a - b);
-
-  console.log(intArr[intArr.length - 2]);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
