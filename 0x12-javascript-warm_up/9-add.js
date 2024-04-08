4209af4311ca275f9793040f864b9a1e7dcac77f
SymonMuchemi <simonkenya846@gmail.com>
2024-04-09 00:04:07 +0300

    Task 9: fix
    Add two integer number args

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,12 +5,13 @@ const num1 = process.argv[2];
 const num2 = process.argv[3];
 
 function add (a, b) {
-  return (0 + a + b);
+  return (a + b);
 }
 
 if (isNaN(num1) === false && isNaN(num2) === false) {
-  const a = parseFloat(num1);
-  const b = parseFloat(num2);
+  const a = parseInt(num1);
+  const b = parseInt(num2);
 
   console.log(add(a, b));
 }
+
