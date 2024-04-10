0f2df2431126abab8143af7fabe22675102de0df
SymonMuchemi <simonkenya846@gmail.com>
2024-04-10 22:59:16 +0300

    used the toString method to handle base conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,20 +1,2 @@
 #!/usr/bin/node
-exports.converter = function (base) {
-  return function (number) {
-    if (base < 2 || base > 16 || isNaN(number)) {
-      return 'Invalid input';
-    }
-
-    const digits = '0123456789ABCDEF';
-    let result = '';
-
-    // divide by base and collect remainders repeatedly
-    while (number > 0) {
-      const rem = number % base;
-      result = digits[rem] + result;
-      number = Math.floor(number / base);
-    }
-
-    return result;
-  };
-};
+exports.converter = function (base) { return num => num.toString(base); };
