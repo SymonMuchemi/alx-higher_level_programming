48d4119c204f8303edd162ba3a5295b72e95e4ca
SymonMuchemi <simonkenya846@gmail.com>
2024-04-10 04:55:17 +0300

    Task 3
    Print method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// empty class
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
+  }
+
+  print() {
+    if (this.width > 0 && this.height > 0) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write('X');
+        }
+      console.log();
+      }
+    }
+  }
+};
