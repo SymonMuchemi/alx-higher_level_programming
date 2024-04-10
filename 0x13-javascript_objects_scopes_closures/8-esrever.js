9f163e570712bb43871c84e0ac72179b3a52f1eb
SymonMuchemi <simonkenya846@gmail.com>
2024-04-10 06:02:34 +0300

    Task 8
    Reversing a list using a loop

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// revereses a list
+exports.esrever = function (list) {
+  const revList = [];
+  let j = 0;
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    revList[j] = list[i];
+    j++;
+  }
+
+  return revList;
+};
