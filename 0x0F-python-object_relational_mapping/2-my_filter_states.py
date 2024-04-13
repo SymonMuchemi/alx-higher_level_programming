890d730f0730250762d7eb2f694efa83fd6f186f
SymonMuchemi <simonkenya846@gmail.com>
2024-04-13 18:45:49 +0300

    task 2 fix
    Formated the query by adding single quotes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,17 +6,17 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
+            host="localhost",
             user=sys.argv[1],
             password=sys.argv[2],
             database=sys.argv[3]
             )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id")
+    cursor.execute("SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id".format(sys.argv[4]))
     rows = cursor.fetchall()
 
     for row in rows:
-        if row[1] == sys.argv[4]:
-            print(row)
+        print(row)
 
     cursor.close()
     db.close()
