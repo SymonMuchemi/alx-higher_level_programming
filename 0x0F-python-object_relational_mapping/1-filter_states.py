199c4390e113c7fdedd7df86cc875abd892d0f85
SymonMuchemi <simonkenya846@gmail.com>
2024-04-13 08:26:05 +0300

    Filter states
    Retrieved and printed states that start with the letter 'N' using the 'LIKE' keyword

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""list all states from the hbtn_0e_0_usa database"""
+import sys
+import MySQLdb
+
+# connecting to the database
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+        )
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    connection.close()
