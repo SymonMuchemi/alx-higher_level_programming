3b7caa5f2aaa2b5aff49aecc55ac476e18c15b57
SymonMuchemi <simonkenya846@gmail.com>
2024-04-14 01:11:32 +0300

    All cities by state
    Retrieve the name of all cities in a state given the state name and using sql JOIN

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""runs a join query from state and cities tables and filter by state"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+        )
+
+    cursor = connection.cursor()
+    query = "SELECT cities.name \
+            FROM cities \
+            JOIN states \
+            ON cities.state_id = states.id \
+            WHERE states.name = %s \
+            ORDER BY cities.id"
+
+    name = (sys.argv[4],)
+    cursor.execute(query, name)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
