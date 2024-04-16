b4fe4085d398dd403dfba755208f19d971e2f88c
SymonMuchemi <simonkenya846@gmail.com>
2024-04-17 02:38:19 +0300

    Get a state
    used .filter().one_or_none() to prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""prints the state with the name passed as an argument"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    nm = sys.argv[1]
+    pw = sys.argv[2]
+    dn = sys.argv[3]
+    name = sys.argv[4]
+    engine = create_engine(
+            f'mysql+mysqldb://{nm}:{pw}@localhost/{dn}',
+            pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == name).one_or_none()
+    if state:
+        print(f"{state.id}: {state.name}")
+    else:
+        print("Not found")
+
+    session.close()
