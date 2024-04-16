18ecf1ba77afbb99a44168cb4b109bb676ba4678
SymonMuchemi <simonkenya846@gmail.com>
2024-04-17 02:13:56 +0300

    First state
    fetched only the first row in the table using .first() instead of .all()

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lists all State objects from the database"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    nm = sys.argv[1]
+    pw = sys.argv[2]
+    dn = sys.argv[3]
+    engine = create_engine(
+            f'mysql+mysqldb://{nm}:{pw}@localhost/{dn}',
+            pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(first_state.id, first_state.name))
+
+    session.close()
