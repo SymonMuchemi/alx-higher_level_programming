bba16378c79ce2cb00cc6d74a141fca91a8d5c55
SymonMuchemi <simonkenya846@gmail.com>
2024-04-17 03:15:13 +0300

    update a state
    fetched the state with the given id and changed its name

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Adds Louisiana to state database adn prints state id"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    nm = sys.argv[1]
+    pw = sys.argv[2]
+    dn = sys.argv[3]
+    engine = create_engine(
+            f'mysql+mysqldb://{nm}:{pw}@localhost/{dn}',
+            pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    if state:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
