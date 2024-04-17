5ed5eb988474eed9432515136e0baa802f43bd7b
SymonMuchemi <simonkenya846@gmail.com>
2024-04-17 03:22:00 +0300

    Delete states
    Looped through each row and deleted it using session.delete(state)

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Deletes all states in the state table"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    nm = sys.argv[1]
+    pw = sys.argv[2]
+    dn = sys.argv[3]
+    engine = create_engine(
+            f'mysql+mysqldb://{nm}:{pw}@localhost/{dn}',
+            pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).all():
+        session.delete(state)
+
+    session.commit()
+
+    session.close()
