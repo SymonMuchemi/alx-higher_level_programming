d3fde3e6bd28d86b46e9d632e79cc24864faf94e
SymonMuchemi <simonkenya846@gmail.com>
2024-04-26 22:06:27 +0300

    only three lines init

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # displays the size of the body of the response
-
-if [ $# -ne 1 ]
-then
-	exit
-else
-	curl --write-out "%{size_download}\n" -s -o /dev/null "$1"
-fi
+curl --write-out "%{size_download}\n" -s -o /dev/null "$1"
