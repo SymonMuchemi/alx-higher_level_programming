405367e121dd9756a12e0bd0e6fae12538cc7096
SymonMuchemi <simonkenya846@gmail.com>
2024-04-26 23:38:58 +0300

    no need for redirections

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a POST req with a set of arguments
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1" -L
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
