ed690ba9ce56b6683567b00dd8d49471c61cdf4e
SymonMuchemi <simonkenya846@gmail.com>
2024-04-27 03:54:03 +0300

    tried and failed

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Finds the peak in a list
+
+def find_peak(list_of_integers):
+    """ finds a peak in the list and print it out """
+    if not list_of_integers:
+        print("None")
+        return
+
+    peaks = []
+    peaks.append(list_of_integers[0])
+    for index in range(1, len(list_of_integers) - 1):
+        current = list_of_integers[index]
+        prev = list_of_integers[index - 1]
+        nxt = list_of_integers[index + 1]
+
+        if current > prev and current > nxt:
+            peaks.append(current)
+    peaks.append(list_of_integers[-1])
+
+    print(max(peaks))
