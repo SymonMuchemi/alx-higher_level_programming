d27fd2d5fb57fca6dc67c2108f7096163027788a
SymonMuchemi <simonkenya846@gmail.com>
2024-04-28 01:32:11 +0300

    used merge sort

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,64 @@
 #!/usr/bin/python3
 """ function finds the peak value in a list on integers
 
-    The list is iterated from first element to the last and the
-    largest value is found and returned
+    The idea is to first sort the array using merge sort which has
+    a time complexity of O(n log n) the retrieve the last element, which is
+    the largest integer element in the array
     """
+
+
+def merge_sort(arr):
+    """sorts the array using divide and conquer
+
+    Args:
+        arr (int): the array of integer elements
+
+    Returns:
+        arr: a sorted version of the initial list
+    """
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+
+def merge(left, right):
+    """combines two arrays to form one
+
+    Args:
+        left (arr): a subarray
+        right (arr): another subarray
+
+    Returns:
+        arr: combined array
+    """
+    result = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    while left_index < len(left):
+        result.append(left[left_index])
+        left_index += 1
+    while right_index < len(right):
+        result.append(right[right_index])
+        right_index += 1
+
+    return result
 
 
 def find_peak(list_of_integers):
@@ -15,15 +70,8 @@ def find_peak(list_of_integers):
     Returns:
         int: the largest integer in the list
     """
-    largest_element = None
-
     if len(list_of_integers) == 0:
         return None
 
-    largest_element = list_of_integers[0]
-
-    for integer in list_of_integers:
-        if integer > largest_element:
-            largest_element = integer
-
-    return largest_element
+    # sort the array and return the last element
+    return merge_sort(list_of_integers)[-1]
