16099abddc1ee2a73484f4e9e1395e356322d509
SymonMuchemi <simonkenya846@gmail.com>
2024-04-28 01:13:48 +0300

    code styled well

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,18 +3,18 @@
 
 def find_peak(list_of_integers):
     """ finds a peak in the list and print it out
-    
+
         Finds the largest element in the list
     """
     largest_element = None
-    
+
     if len(list_of_integers) == 0:
         return None
-    
+
     largest_element = list_of_integers[0]
-    
+
     for integer in list_of_integers:
         if integer > largest_element:
             largest_element = integer
-    
+
     return largest_element
