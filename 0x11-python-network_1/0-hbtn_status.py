6d4c15ac11cf2d95a233f60df72216050bb9d740
SymonMuchemi <simonkenya846@gmail.com>
2024-04-29 02:17:10 +0300

    code not executable on import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ uses urllib to fetch https://alx-intranet.hbtn.io/status """
-import urllib
 import urllib.request
 
 
-location = 'https://alx-intranet.hbtn.io/status'
-# create request body
-req = urllib.request.Request(location)
+if __name__ == "__main__":
+    location = 'https://alx-intranet.hbtn.io/status'
+    # create request body
+    req = urllib.request.Request(location)
 
-with urllib.request.urlopen(req) as response:
-    data = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(data)))
-    print("\t- content: {}".format(data))
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
