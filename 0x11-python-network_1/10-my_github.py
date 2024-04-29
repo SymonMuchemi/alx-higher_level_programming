7bc1d26fb5c637f3976f1ea2d8a2926d1ba834e9
SymonMuchemi <simonkenya846@gmail.com>
2024-04-29 20:12:07 +0300

    used the requests module only

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,11 +4,10 @@
 """
 import sys
 import requests
-from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    auth = (sys.argv[1], sys.argv[2])
     response = requests.get('https://api.github.com/user', auth=auth)
 
     if response.status_code == 200:
