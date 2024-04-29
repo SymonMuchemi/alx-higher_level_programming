9a4e5a97235516c993caae6e0323b69ddb5fd59c
SymonMuchemi <simonkenya846@gmail.com>
2024-04-29 03:48:02 +0300

    What's my status
    used the requests package to fetch a response from the url

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" fetches a url using the requests package """
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url=url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print("\t- content: {}".format(response.text))
