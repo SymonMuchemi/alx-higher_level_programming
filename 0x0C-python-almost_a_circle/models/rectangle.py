de42dcfb86f712880d125bd1a8b1efdbcfa1e0af
symonmuchemi <simonkenya846@gmail.com>
2024-07-16 02:41:57 +0300

    everything documented

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,8 +4,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """Child model to Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
+        """Instantiation"""
+        super().__init__(id=id)
         self.__width = width
         self.__height = height
         self.__x = x
@@ -13,32 +15,72 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """retrieve the width property
+
+        Returns:
+            int: the private width attribute
+        """
         return self.__width
 
     @property
     def height(self):
+        """getter method for height
+
+        Returns:
+            int: private height attribute
+        """
         return self.__height
 
     @property
     def x(self):
+        """getter for x
+
+        Returns:
+            int: private x attribute
+        """
         return self.__x
 
     @property
     def y(self):
+        """getter for y
+
+        Returns:
+            int: private y attrubute
+        """
         return self.__y
 
     @width.setter
     def width(self, value):
+        """changes the values of width
+
+        Args:
+            value (int): new value
+        """
         self.__width = value
 
     @height.setter
     def height(self, value):
+        """changes the values of height
+
+        Args:
+            value (int): new value
+        """
         self.__height = value
 
     @x.setter
     def x(self, value):
+        """changes the values of x
+
+        Args:
+            value (int): new value
+        """
         self.__x = value
 
     @y.setter
     def y(self, value):
+        """changes the values of y
+
+        Args:
+            value (int): new value
+        """
         self.__y = value
