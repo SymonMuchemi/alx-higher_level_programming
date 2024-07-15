3267766ed2acca329bf03cbac4a012df297a17de
symonmuchemi <simonkenya846@gmail.com>
2024-07-16 01:57:48 +0300

    id increments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,7 +7,8 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id == None:
-            self.id = Base.__nb_objects + 1
-        else:
+        if id is not None:
             self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
