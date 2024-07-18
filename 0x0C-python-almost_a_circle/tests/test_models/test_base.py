44ba63290eacb1294ea4646bab0cc034ec885213
symonmuchemi <simonkenya846@gmail.com>
2024-07-18 07:37:55 +0300

    test for base id increments

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -10,17 +10,27 @@ class TestBase(unittest.TestCase):
     Args:
         unittest (TestCase): test cases
     """
-    baseNoId = Base()
-    base_with_id = Base(89)
+    @classmethod
+    def setUpClass(cls):
+        cls.baseNoId = Base()
+        cls.base1 = Base()
+        cls.base2 = Base()
+        cls.base_with_id = Base(89)
 
     def test_base_attributes(self):
         self.assertFalse(hasattr(TestBase.base_with_id, '__nb_objects'))
         self.assertTrue(hasattr(TestBase.base_with_id, 'id'))
 
-    def test_id(self):
+    def test_id_exists(self):
         self.assertIsNotNone(TestBase.base_with_id.id)
         self.assertIsNotNone(TestBase.baseNoId.id)
+
+    def test_id_as_instantiation(self):
         self.assertEqual(TestBase.base_with_id.id, 89)
+
+    def test_id_increment_by_1(self):
+        diff = TestBase.base2.id - TestBase.base1.id
+        self.assertEqual(1, diff)
 
 
 if __name__ == "__main__":
