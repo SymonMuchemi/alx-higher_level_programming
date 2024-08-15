55dd6270ec14a662ac493be566134304fcf70bf9
symonmuchemi <simonkenya846@gmail.com>
2024-08-16 02:45:12 +0300

    task: enumerate films with actor

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// get the number of movies where a character is present
+const { count } = require('console');
+const request = require('request');
+
+const url = process.argv[2];
+
+request(`${url}`, function (error, resp, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const results = JSON.parse(body).results;
+
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
