37348cd74dedf4ba09ad4bfdcb0cb2725384ae0a
symonmuchemi <simonkenya846@gmail.com>
2024-08-16 01:17:01 +0300

    if this is acceptable, I will be very motivated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-// reads and prints the content of a file
 const fs = require('fs');
-const path = process.argv[1];
 
-fs.readFile(path, 'utf-8', (error, data) => {
-  if (error) {
-    console.error(error);
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
   }
-  console.log(data);
 });
