bd51796423e27f3cf717c9eb83647dbe43e9552f
symonmuchemi <simonkenya846@gmail.com>
2024-08-21 19:57:24 +0300

    before semistandard:

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+// prints all characters of a star wars movie
+const request = require('request')
+
+const movieId = process.argv[2]
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId
+
+function getCharacterName(url) {
+    request(url, function(err, response, body){
+        if (err) {
+            console.log(err)
+            return null
+        }
+
+        const data = JSON.parse(body)
+        console.log(data.name);
+    })
+}
+
+request(url, function(error, response, body){
+    if (error) {
+        console.log(err)
+        return null
+    }
+
+    const data = JSON.parse(body);
+    const names = data.characters;
+
+    for (let name of names) {
+        getCharacterName(name)
+    }
+})
