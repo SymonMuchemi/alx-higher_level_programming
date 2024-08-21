22e7f410b70c0029f431f149eee1f7ad78ede972
symonmuchemi <simonkenya846@gmail.com>
2024-08-21 19:58:44 +0300

    task: print characters in a movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,32 +1,32 @@
 #!/usr/bin/node
 // prints all characters of a star wars movie
-const request = require('request')
+const request = require('request');
 
-const movieId = process.argv[2]
-const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId
+const movieId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-function getCharacterName(url) {
-    request(url, function(err, response, body){
-        if (err) {
-            console.log(err)
-            return null
-        }
-
-        const data = JSON.parse(body)
-        console.log(data.name);
-    })
-}
-
-request(url, function(error, response, body){
-    if (error) {
-        console.log(err)
-        return null
+function getCharacterName (url) {
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log(err);
+      return null;
     }
 
     const data = JSON.parse(body);
-    const names = data.characters;
+    console.log(data.name);
+  });
+}
 
-    for (let name of names) {
-        getCharacterName(name)
-    }
-})
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return null;
+  }
+
+  const data = JSON.parse(body);
+  const names = data.characters;
+
+  for (const name of names) {
+    getCharacterName(name);
+  }
+});
